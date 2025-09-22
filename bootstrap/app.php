09797e1de75287aac5d9d9d6c47eabd75eda6c9b
Shafiq Alshaar <shafiqalshaar@gmail.com>
2025-09-22 14:33:41 +0400

    xml response

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
             'door/*',
             'data/*',
             'webhook/*',
+            'iclock/*',
+            'icdata/*',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
